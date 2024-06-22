68e7ed25971ee443780737d6c280d907b07752f9
Pagal3 <P4gal3@protonmail.com>
2024-06-22 20:19:33 +0300

    Edit template

--- template.cs
+++ template.cs
@@ -1,68 +1,65 @@
-class Program 
+string[] lines; // the input is saved here
+
+string GetInputFilePath(string type_of_input_file)
 {
-	static string[] lines; // the input is saved here
+	if (!type_of_input_file.EndsWith(".txt"))
+		type_of_input_file += ".txt";
 
-    static string GetInputFilePath(string type_of_input_file)
-    {
-        if (!type_of_input_file.EndsWith(".txt"))
-            type_of_input_file += ".txt";
+	string current_exe_directory = Environment.CurrentDirectory;
+	string path_to_input = Path.GetFullPath(Path.Combine(current_exe_directory, @"..\..\..\..\..\..\"));
 
-        string current_exe_directory = Environment.CurrentDirectory;
-        string path_to_input = Path.GetFullPath(Path.Combine(current_exe_directory, @"..\..\..\..\..\..\"));
-
-        
-        foreach (var path in Directory.GetFiles(path_to_input))
-        {
-            //Console.WriteLine(path); // full path
-            //Console.WriteLine(System.IO.Path.GetFileName(path)); // file name
-
-            if (System.IO.Path.GetFileName(path) == type_of_input_file)
-                return path;
-        }
-
-        Console.WriteLine($"ERROR: Specified input file \"{type_of_input_file}\" not found in directory \"{path_to_input}\"");
-
-        return null;
-        
-    }
-
-    static void ReadInput(string type_of_input_file)
-    {
-        string path_to_input = GetInputFilePath(type_of_input_file);
-
-        if (path_to_input == null)
-            Environment.Exit(1);
-
-        lines = File.ReadAllLines(path_to_input);
-        
-
-
-    }
 	
-
-	static void Part1()
+	foreach (var path in Directory.GetFiles(path_to_input))
 	{
-		
+		//Console.WriteLine(path); // full path
+		//Console.WriteLine(System.IO.Path.GetFileName(path)); // file name
 
-
-		
-	}
-	
-	static void Part2()
-	{
-		
-
-
-		
+		if (System.IO.Path.GetFileName(path) == type_of_input_file)
+			return path;
 	}
 
-	static void Main()
-    {
-		ReadInput("input");
-		
-        Part1();
-        // Part2();
-    }
+	Console.WriteLine($"ERROR: Specified input file \"{type_of_input_file}\" not found in directory \"{path_to_input}\"");
+
+	return null;
+	
+}
+
+void ReadInput(string type_of_input_file)
+{
+	string path_to_input = GetInputFilePath(type_of_input_file);
+
+	if (path_to_input == null)
+		Environment.Exit(1);
+
+	lines = File.ReadAllLines(path_to_input);
+	
+
+
+}
+
+
+void Part1()
+{
+	
+
+
+	
+}
+
+void Part2()
+{
+	
+
+
+	
+}
+
+void Main()
+{
+	ReadInput("input");
+	
+	Part1();
+	// Part2();
 }
 
 
